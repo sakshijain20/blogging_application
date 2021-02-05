aae4c1535b2ed5a1406325b9b263030b94580cc9
Sakshi Madrecha <jain.sakshi1099@gmail.com>
2021-02-06 00:30:11 +0530

    added methods

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,42 @@
 class SessionController < ApplicationController
+
+  def new
+    	user = User.new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    # if user && user.password == ( params[:session][:password] )
+    # Log the user in and redirect to the user's show page.
+    # @token = @user.auth_token
+    # user.auth_token = SecureRandom.hex
+    # session[ :user_id ] = user.id
+    # user.save
+    # session[ :auth_token ] = user.auth_token
+    flash[:success] = 'Logged in successfully!'
+    redirect_to root_url
+
+    # else
+    # Create an error message.
+    # flash[ :danger ] = "Invalid email/password combination"
+    # render 'new'
+    end
+
+	def destroy
+    	if @user
+	      @user.auth_token = ''
+	      @user.save
+	      respond_to do |format|
+	        format.json { render json: { status: '200', message: 'logged out successfully' } }
+	      end
+	  end
+	  
+
+    end
+  
+
+  def edit; end
+
+  def update; end
+
 end
