5b0f8ec670cca3cdc820b9f96d94ea1abda8c538
Sakshi Madrecha <jain.sakshi1099@gmail.com>
2021-02-05 23:31:29 +0530

    changed permission in params

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -64,6 +64,6 @@ class BlogsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def blog_params
-      params.require(:blog).permit(:title, :readtime, :category_id, :user_id, :image, :content)
+      params.require(:blog).permit(:title, :readtime, :category_id, :user_id, :content)
     end
 end
